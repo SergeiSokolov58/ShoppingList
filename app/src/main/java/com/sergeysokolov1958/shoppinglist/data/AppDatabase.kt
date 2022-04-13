78cd2dd5b78826377841aae3e83bae2f49d73d44
sergeysokolov1958 <sergeysokolov1958@gmail.com>
2022-04-14 00:25:57 +0300

    Fixed error in ShopItem.kt

--- app/src/main/java/com/sergeysokolov1958/shoppinglist/data/AppDatabase.kt
+++ app/src/main/java/com/sergeysokolov1958/shoppinglist/data/AppDatabase.kt
@@ -7,7 +7,7 @@ import androidx.room.RoomDatabase
 
 
 @Database(entities = [ShopItemDbModel::class], version = 1, exportSchema = false)
-abstract class AppDatabase: RoomDatabase() {
+abstract class AppDatabase : RoomDatabase() {
 
     abstract fun shopListDao(): ShopListDao
 
@@ -29,7 +29,9 @@ abstract class AppDatabase: RoomDatabase() {
                     application,
                     AppDatabase::class.java,
                     DB_NAME
-                ).build()
+                )
+                    .allowMainThreadQueries()
+                    .build()
                 INSTANCE = db
                 return db
             }
--- app/src/main/java/com/sergeysokolov1958/shoppinglist/presentation/MainViewModel.kt
+++ app/src/main/java/com/sergeysokolov1958/shoppinglist/presentation/MainViewModel.kt
@@ -1,5 +1,7 @@
 package com.sergeysokolov1958.shoppinglist.presentation
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.sergeysokolov1958.shoppinglist.data.ShopListRepositoryImpl
@@ -8,9 +10,9 @@ import com.sergeysokolov1958.shoppinglist.domain.EditShopItemUseCase
 import com.sergeysokolov1958.shoppinglist.domain.GetShopListUseCase
 import com.sergeysokolov1958.shoppinglist.domain.ShopItem
 
-class MainViewModel : ViewModel() {
+class MainViewModel(application: Application) : AndroidViewModel(application) {
 
-    private val repository = ShopListRepositoryImpl
+    private val repository = ShopListRepositoryImpl(application)
 
     private val getShopListUseCase = GetShopListUseCase(repository)
     private val deleteShopItemUseCase = DeleteShopItemUseCase(repository)
